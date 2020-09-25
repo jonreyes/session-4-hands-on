d58bbb483452f191a63b746b2840c8f997baeb17
askyang <yang.he@askapplications.com>
2020-09-25 03:19:30 +0000

    hands-on-1-solution

---
+++ src/Car.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+class Car extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      brand: "Ford",
+      model: "Mustang",
+      color: "red",
+      year: 1964
+    };
+  }
+  changeColor = () => {
+    this.setState({color: "blue"});
+  }
+  render() {
+    return (
+      <div className="car">
+        <h1>My {this.state.brand}</h1>
+        <p>
+          It is a {this.state.color} {this.state.model} from {this.state.year}.
+        </p>
+        <button
+          type="button"
+          onClick={this.changeColor}
+        >Change color</button>
+      </div>
+    );
+  }
+}
+
+
+export default Car;
