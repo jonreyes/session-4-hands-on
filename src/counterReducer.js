3decf51cf794ba37c8c0724fbfe1a098d47124a9
askyang <yang.he@askapplications.com>
2020-09-30 04:42:51 +0000

    update

--- src/counterReducer.js
+++ src/counterReducer.js
@@ -1,29 +1,15 @@
-export function increment() {
-  //  return {
-  //     type: 'INCREMENT'
-  //  }
-}
-export function decrement() {
-  //  return {
-  //     type: 'DECREMENT'
-  //  }
-}
-export function reset() {
-  //  return { type: 'RESET' }
-}
-
 const initialState = {
-  // count: 0
+  // initiate count as 0
 };
 
 const counterReducer = (state = initialState, action) => {
    switch (action.type) {
       case 'INCREMENT': 
-        // return { count: state.count + 1 };
+        // return new count with increment
       case 'DECREMENT': 
-        // return { count: state.count - 1 };
+        // return new count with decrement
       case 'RESET' : 
-        // return { count: 0 }
+        // return count as 0
       default: 
         return state;
    }
